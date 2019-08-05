3299b7a9c87d46f9fa28b1541939edeb165bb918
James-Livesey <jamesliveg@gmail.com>
2019-08-05 16:03:13 +0100

    Add root site

---
+++ build/graph.js
@@ -0,0 +1,1 @@
+var _assets={};function setup(){var canvas=createCanvas(window.innerWidth,window.innerHeight-70);canvas.parent("graphCanvas");background(255,0,200);}
---
+++ root/graph.js
@@ -0,0 +1,7 @@
+function setup() {
+    var canvas = createCanvas(window.innerWidth, window.innerHeight - 70);
+
+    canvas.parent("graphCanvas");
+
+    background(255, 0, 200);
+}
