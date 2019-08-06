26434941c5919af39757467b5a0143ccaa2614f9
James-Livesey <jamesliveg@gmail.com>
2019-08-06 11:52:36 +0100

    Add cool graph

--- root/graph.js
+++ root/graph.js
@@ -1,69 +1,36 @@
-class Snake {
-    constructor(initialX, initialY, colour, plumpiness, maxLength) {
-        this.x = initialX;
-        this.y = initialY;
-        this.colour = colour;
-        this.plumpiness = plumpiness;
-        this.maxLength = maxLength;
-
-        this.positionMemory = [[initialX, initialY]];
-    }
-
-    draw() {
-        if (this.positionMemory.length < 2) {
-            fill(... this.colour);
-            
-            circle(this.x, this.y, this.plumpiness, this.plumpiness);
-        } else {
-
-            for (var i = 0; i < this.positionMemory.length; i++) {
-                fill(
-                    Math.min(this.colour[0] + (255 * ((this.positionMemory.length - i) / this.positionMemory.length)), 255),
-                    Math.min(this.colour[1] + (255 * ((this.positionMemory.length - i) / this.positionMemory.length)), 255),
-                    Math.min(this.colour[2] + (255 * ((this.positionMemory.length - i) / this.positionMemory.length)), 255)
-                );
-                
-                circle(this.positionMemory[i][0], this.positionMemory[i][1], this.plumpiness, this.plumpiness);
-            }
-        }
-    }
-
-    translate(x, y) {
-        this.positionMemory.push([this.x + x, this.y + y]);
-
-        if (this.positionMemory.length > this.maxLength) {
-            this.positionMemory.shift();
-        }
-
-        this.x = this.positionMemory[this.positionMemory.length - 1][0];
-        this.y = this.positionMemory[this.positionMemory.length - 1][1];
-    }
-}
-
-var snakey = new Snake(0, window.innerHeight / 2, [255, 0, 0], 30, 500);
+var data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 function setup() {
     var canvas = createCanvas(window.innerWidth, window.innerHeight - 70);
 
     canvas.parent("graphCanvas");
     rectMode(CENTER);
-    noStroke();
 }
 
 function draw() {
     background(255);
 
-    snakey.draw();
+    stroke(255, 0, 0);
+    strokeWeight(5);
 
-    if (keyIsDown(87)) {
-        snakey.translate(0, -2);
-    } else if (keyIsDown(83)) { 
-        snakey.translate(0, 2);
-    }
+    var previousData = (window.innerHeight - 70) * (1 - (data[i] / 2));
 
-    if (keyIsDown(65)) {
-        snakey.translate(-2, 0);
-    } else if (keyIsDown(68)) {
-        snakey.translate(2, 0);
+    for (var i = 0; i < 10; i++) {
+        console.log(window.innerWidth * (i / 12));
+        
+        line((window.innerWidth * (i / 12)) - (window.innerWidth / 12), previousData, (window.innerWidth * ((i + 1) / 12)) - (window.innerWidth / 12), (window.innerHeight - 70) * (1 - (data[i] / 2)));
+
+        previousData = (window.innerHeight - 70) * (1 - (data[i] / 2));
     }
 }
+
+setInterval(function() {
+    var dataIteration = getDummyData("GBP", "EUR", 10);
+
+    var part = dataIteration["Time Series FX (Monthly)"];
+    var key = Object.keys(part)[0];
+    
+    data.push(part[key]["4. close"]);
+
+    data = data.slice(Math.max(data.length - 10, 1));
+}, 500);
