f367d8d2f43be34adc148ffaa255b422497ea826
William Jones <untangledwilliam@gmail.com>
2019-08-06 14:49:58 +0100

    We created the function that returns the data object, using async/await.

--- API/main.js
+++ API/main.js
@@ -6,9 +6,15 @@ const API_KEY = "8KOBOUBTEF7RHR21";
 
 // FUNCTIONS
 
-function transformAPIData(data, rootKey) {
-  let dataValuesArray = Object
-    .entries(data[rootKey])
+async function getExchangeRate(from, to) {
+  const requestURL = `https://www.alphavantage.co/query?function=FX_MONTHLY&from_symbol=${from}&to_symbol=${to}&apikey=${API_KEY}`;
+
+  // You can only do 5 calls per minute
+
+  const response = await fetch(requestURL);
+  const data = await response.json();
+
+  const dataValuesArray = Object.entries(data["Time Series FX (Monthly)"])
     .sort(function(a, b) {
       return new Date(a[0]) - new Date(b[0]);
     })
@@ -16,56 +22,36 @@ function transformAPIData(data, rootKey) {
       return parseFloat(item[1]["4. close"]);
     });
 
-    return dataValuesArray;
+  console.log(dataValuesArray);
+
+  return dataValuesArray;
 }
 
-function getExchangeRate(from, to) {
-  let request = new XMLHttpRequest();
+async function getStonks(symbol) {
+  const requestURL = `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=${symbol}&apikey=${API_KEY}`;
 
-  let requestURL = `https://www.alphavantage.co/query?function=FX_MONTHLY&from_symbol=${from}&to_symbol=${to}&apikey=${API_KEY}`;
-  
-  request.open('GET', requestURL, true);
-  
-  request.onload = function() {
-    //Begin accessing JSON data here
-    let data = JSON.parse(this.response);
+  const response = await fetch(requestURL);
+  const data = await response.json();
 
-    let dataValuesArray = transformAPIData(data, "Time Series FX (Monthly)");
+  const dataValuesArray = Object.entries(data["Monthly Time Series"])
+    .sort(function(a, b) {
+      return new Date(a[0]) - new Date(b[0]);
+    })
+    .map(function(item) {
+      return {
+        sharePrice: parseFloat(item[1]["4. close"]),
+        volume: parseInt(item[1]["5. volume"])
+      };
+    });
 
-    console.log(dataValuesArray);
+  console.log(dataValuesArray);
 
-    return dataValuesArray;
-  }
-
-  request.send();
-  
-}
-
-function getStonks(symbol) {
-  let request = new XMLHttpRequest();
-
-  let requestURL = `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=${symbol}&apikey=${API_KEY}`;
-  
-  request.open('GET', requestURL, true);
-
-  request.onload = function() {
-    //Begin accessing JSON data here
-    let data = JSON.parse(this.response);
-
-    let dataValuesArray = transformAPIData(data, "Monthly Time Series");
-    
-    console.log(dataValuesArray);
-
-    return dataValuesArray;
-  }
-
-  request.send();
-  
+  return dataValuesArray;
 }
 
 // Get the data from the user's input
 
-document.getElementById("stonkForm").addEventListener('submit', function(e) {
+document.getElementById("stonkForm").addEventListener("submit", function(e) {
   e.preventDefault();
   let fromCode = document.getElementById("fromCurrencyCode").value;
   let toCode = document.getElementById("toCurrencyCode").value;
@@ -76,27 +62,50 @@ document.getElementById("stonkForm").addEventListener('submit', function(e) {
 // Get all the currencies from the file
 let currencyRequest = new XMLHttpRequest();
 
-currencyRequest.open('GET', 'https://www.alphavantage.co/physical_currency_list/', true);
+currencyRequest.open(
+  "GET",
+  "https://www.alphavantage.co/physical_currency_list/",
+  true
+);
 
 currencyRequest.onload = function() {
   //Begin accessing JSON data here
   let data = this.response;
 
-  let dataArray = data.split('\n');
+  let dataArray = data.split("\n");
   dataArray.shift();
   dataArray.pop();
 
   dataArray.forEach(function(item) {
     let option = document.createElement("option");
 
-    option.value = item.split(',')[0];
-    option.innerHTML = item.split(',')[1];
+    option.value = item.split(",")[0];
+    option.innerHTML = item.split(",")[1];
 
     document.getElementById("fromCurrencyCode").appendChild(option);
-    document.getElementById("toCurrencyCode").appendChild(option.cloneNode(true));
+    document
+      .getElementById("toCurrencyCode")
+      .appendChild(option.cloneNode(true));
   });
-}
+};
 
 currencyRequest.send();
 
-getStonks("MSFT");
+// call this with async / await
+async function getData() {
+  return {
+    google: await getStonks("GOOG"),
+    microsoft: await getStonks("MSFT"),
+    apple: await getStonks("AAPL"),
+    wetherspoons: await getStonks("JDW.LON"),
+    GBPtoUSD: await getExchangeRate("GBP", "USD"),
+    GBPtoEUR: await getExchangeRate("GBP", "EUR")
+  };
+}
+
+async function getAsyncData() {
+  const data = await getData();
+  console.log(data);
+}
+
+getAsyncData();
