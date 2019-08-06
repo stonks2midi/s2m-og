ea29199a0b19fa2c3a542872d12c9e368cbd7850
William Jones <untangledwilliam@gmail.com>
2019-08-06 10:45:41 +0100

    I got the currencies from the list on the API's website, and now the user can choose which currencies they want to convert. This is then passed into the API request, and the response is logged in the console.

--- API/main.js
+++ API/main.js
@@ -1,11 +1,75 @@
+// VARIABLES
+
+// Constants
+
+const API_KEY = "8KOBOUBTEF7RHR21";
+
+// FUNCTIONS
+
+function getExchangeRate(from, to) {
+  let request = new XMLHttpRequest();
+
+  let requestURL = `https://www.alphavantage.co/query?function=FX_MONTHLY&from_symbol=${from}&to_symbol=${to}&apikey=8KOBOUBTEF7RHR21`;
+  console.log(requestURL);
+  
+  request.open('GET', requestURL, true);
+
+  console.log('stonk function called');
+  
+  request.onload = function() {
+    //Begin accessing JSON data here
+    let data = JSON.parse(this.response);
+    console.log(data);
+
+    // let exchangeRate = data["Realtime Currency Exchange Rate"]["5. Exchange Rate"];
+    // console.log(exchangeRate);
+  }
+
+  request.send();
+}
+
 // Get the data from the user's input
 
-const API_KEY = "8KOBOUBTEF7RHR21"
+document.getElementById("stonkForm").addEventListener('submit', function(e) {
+  e.preventDefault();
+  console.log('from and to currency submitted');
+  let fromCode = document.getElementById("fromCurrencyCode").value;
+  console.log(fromCode);
+  let toCode = document.getElementById("toCurrencyCode").value;
+  console.log(toCode);
 
-$('stonkForm').submit(function() {
-  fromCurrencyCode = $('fromCurrencyCode').val();
-  toCurrencyCode = $('toCurrencyCode').val();
+  getExchangeRate(fromCode, toCode);
 });
+
+// Get all the currencies from the file
+let currencyRequest = new XMLHttpRequest();
+
+currencyRequest.open('GET', 'https://www.alphavantage.co/physical_currency_list/', true);
+
+currencyRequest.onload = function() {
+  //Begin accessing JSON data here
+  console.log(this.response);
+  let data = this.response;
+  console.log(data);
+
+  let dataArray = data.split('\n');
+  dataArray.shift();
+  dataArray.pop();
+
+  console.log(dataArray);
+
+  dataArray.forEach(function(item) {
+    let option = document.createElement("option");
+
+    option.value = item.split(',')[0];
+    option.innerHTML = item.split(',')[1];
+
+    document.getElementById("fromCurrencyCode").appendChild(option);
+    document.getElementById("toCurrencyCode").appendChild(option.cloneNode(true));
+  });
+}
+
+currencyRequest.send();
 
 // Get the data from the stonk API
 let request = new XMLHttpRequest();
@@ -17,10 +81,25 @@ request.onload = function() {
   let data = JSON.parse(this.response);
   console.log(data);
 
-  let exchangeRate = data["Realtime Currency Exchange Rate"]["5. Exchange Rate"];
-  console.log(exchangeRate);
+  // let exchangeRate = data["Realtime Currency Exchange Rate"]["5. Exchange Rate"];
+  // console.log(exchangeRate);
 }
 
 request.send();
 
 $('exchangeRate').innerHTML = exchangeRate;
+
+/**
+ * Gets currency data from the alpha advantage API
+ * 
+ * @param {*} from 
+ * @param {*} to 
+ * 
+ * @returns JSON
+ */
+function getCurrencyData(from = "GBP", to = "EUR") {
+  // Build the url for the api using the symbols passed in the function prototype
+  var url = ''; 
+
+  // 
+}
